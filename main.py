76b3325e5ac390ddd018aaafabaebd191ad85a50
lsy0420 <dg2026ai00@danggok.hs.kr>
2026-03-16 11:03:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,291 +1,193 @@
 import streamlit as st
-
- 
-
-# 페이지 설정
+import random
 
 st.set_page_config(
-
-    page_title="MBTI 청소년 문학 추천 ",
-
-    page_icon="",
-
+    page_title="MBTI 청소년 문학 & 포켓몬 추천",
+    page_icon="📚",
     layout="centered"
-
 )
 
- 
+st.title("📚 MBTI 청소년 문학 & 포켓몬 추천")
+st.write("🧠 MBTI를 선택하면 **어울리는 청소년 문학 도서와 포켓몬**을 추천해드립니다! ✨")
 
-# 제목
+st.divider()
 
-st.title(" MBTI 청소년 문학 추천")
+mbti_types = [
+"INTJ","INTP","ENTJ","ENTP",
+"INFJ","INFP","ENFJ","ENFP",
+"ISTJ","ISFJ","ESTJ","ESFJ",
+"ISTP","ISFP","ESTP","ESFP"
+]
 
-st.subheader(" 나의 MBTI에 맞는 청소년 문학을 찾아보세요!")
+selected_mbti = st.selectbox("🔎 MBTI를 선택하세요", mbti_types)
 
- 
+data = {
 
-st.write("MBTI 유형을 선택하면 어울리는 청소년 문학을 추천해드립니다 ")
-
- 
-
-# MBTI 선택
-
-mbti = st.selectbox(
-
-    " MBTI 유형을 선택하세요",
-
-    [
-
-        "INTJ","INTP","ENTJ","ENTP",
-
-        "INFJ","INFP","ENFJ","ENFP",
-
-        "ISTJ","ISFJ","ESTJ","ESFJ",
-
-        "ISTP","ISFP","ESTP","ESFP"
-
-    ]
-
-)
-
- 
-
-# 추천 도서 데이터
-
-books = {
-
- 
-
-"INTJ": {
-
-"title":"모모",
-
+"INTJ":{
+"title":"📘 모모",
 "author":"미하엘 엔데",
-
-"desc":"시간을 훔치는 사람들에 맞서 싸우는 소녀 모모의 이야기. 깊은 사유와 철학적 메시지가 INTJ에게 잘 맞습니다."
-
+"story":"시간을 빼앗는 회색 신사들에 맞서는 소녀 모모의 이야기.",
+"pokemon":"뮤츠",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
+"poke_reason":"뮤츠는 지능적이고 전략적인 포켓몬으로 깊이 생각하는 INTJ와 잘 어울립니다."
 },
 
- 
-
-"INTP": {
-
-"title":"어린 왕자",
-
+"INTP":{
+"title":"📗 어린 왕자",
 "author":"생텍쥐페리",
-
-"desc":"세상과 인간을 깊이 생각하게 만드는 철학적 이야기."
-
+"story":"여러 행성을 여행하며 삶의 의미를 탐구하는 이야기.",
+"pokemon":"후딘",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/65.png",
+"poke_reason":"후딘은 높은 지능과 분석력을 가진 포켓몬으로 탐구형 INTP와 잘 맞습니다."
 },
 
- 
-
-"ENTJ": {
-
-"title":"헝거게임",
-
+"ENTJ":{
+"title":"📕 헝거 게임",
 "author":"수잔 콜린스",
-
-"desc":"강한 리더십과 전략적 사고가 필요한 상황 속에서 성장하는 이야기."
-
+"story":"독재 사회에서 살아남기 위해 싸우는 소녀의 이야기.",
+"pokemon":"리자몽",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+"poke_reason":"리자몽은 강력한 리더 느낌의 포켓몬으로 카리스마 있는 ENTJ와 잘 어울립니다."
 },
 
- 
-
-"ENTP": {
-
-"title":"셜록 홈즈",
-
-"author":"아서 코난 도일",
-
-"desc":"기발한 추리와 논리적 사고가 돋보이는 탐정 이야기."
-
+"ENTP":{
+"title":"📙 퍼시 잭슨",
+"author":"릭 라이어던",
+"story":"그리스 신화 세계에서 벌어지는 모험 이야기.",
+"pokemon":"팬텀",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png",
+"poke_reason":"장난기 많고 창의적인 팬텀은 아이디어가 넘치는 ENTP와 비슷합니다."
 },
 
- 
-
-"INFJ": {
-
-"title":"나미야 잡화점의 기적",
-
-"author":"히가시노 게이고",
-
-"desc":"사람들의 고민을 통해 삶의 의미를 찾는 따뜻한 이야기."
-
+"INFJ":{
+"title":"📘 책 도둑",
+"author":"마커스 주삭",
+"story":"전쟁 속에서도 책과 희망을 지키는 이야기.",
+"pokemon":"루기아",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png",
+"poke_reason":"루기아는 신비롭고 깊은 성격을 가진 포켓몬으로 이상주의적 INFJ와 잘 맞습니다."
 },
 
- 
+"INFP":{
+"title":"📗 테라비시아로 가는 다리",
+"author":"캐서린 패터슨",
+"story":"상상 속 왕국을 만들며 성장하는 아이들의 이야기.",
+"pokemon":"이브이",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png",
+"poke_reason":"이브이는 가능성과 상상력이 많은 포켓몬으로 감성적인 INFP와 어울립니다."
+},
 
-"INFP": {
+"ENFJ":{
+"title":"📕 원더",
+"author":"R.J 팔라시오",
+"story":"차이를 극복하고 친구들과 성장하는 이야기.",
+"pokemon":"피카츄",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+"poke_reason":"피카츄는 모두에게 사랑받는 포켓몬으로 따뜻한 ENFJ와 잘 맞습니다."
+},
 
-"title":"빨강머리 앤",
-
+"ENFP":{
+"title":"📙 빨간 머리 앤",
 "author":"루시 모드 몽고메리",
-
-"desc":"상상력 풍부한 앤의 성장 이야기."
-
+"story":"상상력이 풍부한 앤의 성장 이야기.",
+"pokemon":"토게피",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/175.png",
+"poke_reason":"밝고 긍정적인 토게피는 자유로운 ENFP와 어울립니다."
 },
 
- 
-
-"ENFJ": {
-
-"title":"페인트",
-
-"author":"이희영",
-
-"desc":"청소년의 정체성과 가족에 대한 깊은 질문을 던지는 소설."
-
+"ISTJ":{
+"title":"📘 작은 아씨들",
+"author":"루이자 메이 올컷",
+"story":"가족과 책임을 중심으로 한 성장 이야기.",
+"pokemon":"거북왕",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+"poke_reason":"차분하고 안정적인 거북왕은 책임감 있는 ISTJ와 비슷합니다."
 },
 
- 
-
-"ENFP": {
-
-"title":"완득이",
-
-"author":"김려령",
-
-"desc":"유쾌하고 따뜻한 성장 이야기."
-
+"ISFJ":{
+"title":"📗 키다리 아저씨",
+"author":"진 웹스터",
+"story":"따뜻한 도움 속에서 성장하는 이야기.",
+"pokemon":"해피너스",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/242.png",
+"poke_reason":"해피너스는 다른 포켓몬을 돌보는 성격이라 ISFJ와 잘 맞습니다."
 },
 
- 
-
-"ISTJ": {
-
-"title":"마당을 나온 암탉",
-
-"author":"황선미",
-
-"desc":"책임감과 신념을 보여주는 감동적인 이야기."
-
-},
-
- 
-
-"ISFJ": {
-
-"title":"구름빵",
-
-"author":"백희나",
-
-"desc":"따뜻한 가족 이야기가 담긴 작품."
-
-},
-
- 
-
-"ESTJ": {
-
-"title":"데미안",
-
-"author":"헤르만 헤세",
-
-"desc":"자기 성장과 삶의 방향을 찾는 이야기."
-
-},
-
- 
-
-"ESFJ": {
-
-"title":"Wonder (원더)",
-
-"author":"R.J. 팔라시오",
-
-"desc":"다름을 이해하고 함께 살아가는 이야기."
-
-},
-
- 
-
-"ISTP": {
-
-"title":"로빈슨 크루소",
-
+"ESTJ":{
+"title":"📕 로빈슨 크루소",
 "author":"다니엘 디포",
-
-"desc":"생존과 문제 해결 능력이 돋보이는 모험 이야기."
-
+"story":"무인도에서 살아남는 생존 이야기.",
+"pokemon":"마기라스",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/248.png",
+"poke_reason":"강력하고 리더십 있는 마기라스는 ESTJ와 비슷합니다."
 },
 
- 
-
-"ISFP": {
-
-"title":"바람의 그림자",
-
-"author":"카를로스 루이스 사폰",
-
-"desc":"감성적인 분위기의 미스터리 성장 이야기."
-
+"ESFJ":{
+"title":"📙 비밀의 화원",
+"author":"프랜시스 버넷",
+"story":"정원을 통해 아이들이 변화하는 이야기.",
+"pokemon":"님피아",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/700.png",
+"poke_reason":"님피아는 관계와 유대를 중요하게 여겨 ESFJ와 잘 맞습니다."
 },
 
- 
-
-"ESTP": {
-
-"title":"해리포터",
-
-"author":"J.K. 롤링",
-
-"desc":"모험과 도전이 가득한 판타지 이야기."
-
+"ISTP":{
+"title":"📘 손도끼",
+"author":"게리 폴슨",
+"story":"자연 속에서 생존하는 소년 이야기.",
+"pokemon":"루카리오",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png",
+"poke_reason":"루카리오는 조용하지만 강한 전투 스타일로 ISTP와 비슷합니다."
 },
 
- 
+"ISFP":{
+"title":"📗 별을 헤아리며",
+"author":"로이스 로리",
+"story":"전쟁 속에서도 친구를 지키려는 이야기.",
+"pokemon":"세레비",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/251.png",
+"poke_reason":"자연과 감성을 가진 세레비는 예술가형 ISFP와 잘 어울립니다."
+},
 
-"ESFP": {
+"ESTP":{
+"title":"📕 80일간의 세계일주",
+"author":"쥘 베른",
+"story":"세계 여행을 하는 모험 이야기.",
+"pokemon":"갸라도스",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png",
+"poke_reason":"강력하고 활동적인 갸라도스는 행동가 ESTP와 잘 맞습니다."
+},
 
-"title":"찰리와 초콜릿 공장",
-
+"ESFP":{
+"title":"📙 찰리와 초콜릿 공장",
 "author":"로알드 달",
-
-"desc":"즐거움과 상상력이 가득한 이야기."
+"story":"초콜릿 공장에서 벌어지는 환상 이야기.",
+"pokemon":"푸린",
+"poke_img":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png",
+"poke_reason":"귀엽고 사람을 즐겁게 하는 푸린은 ESFP와 잘 어울립니다."
+}
 
 }
 
- 
+st.divider()
 
-}
+if st.button("📖 추천 보기"):
 
- 
-
-# 버튼
-
-if st.button(" 도서 추천 받기"):
-
- 
+    info = data[selected_mbti]
 
     st.balloons()
+    st.snow()
 
- 
+    st.subheader(info["title"])
+    st.write("✍ 작가:", info["author"])
 
-    book = books[mbti]
+    st.markdown("### 📚 줄거리")
+    st.write(info["story"])
 
- 
+    st.markdown("### 🐾 어울리는 포켓몬")
+    st.image(info["poke_img"], width=200)
 
-    st.success(f" {mbti}에게 추천하는 책!")
+    st.markdown("### ⭐ 포켓몬 추천 이유")
+    st.info(info["poke_reason"])
 
- 
-
-    st.markdown(f"""
-
-###  {book['title']}
-
-** 작가:** {book['author']}
-
- 
-
- **추천 이유**
-
- 
-
-{book['desc']}
-
-""")
-
- 
-
-    st.info(" 이 책은 MBTI 성향을 바탕으로 추천된 청소년 문학입니다.")
+st.caption("📚 MBTI 청소년 문학 + 포켓몬 추천 앱")
